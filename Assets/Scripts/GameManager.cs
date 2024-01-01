85776540850d991aedab81f1c333a019bc47e50e
Hamed Hassani <hamed2008ir@gmail.com>
2024-01-01 15:22:31 +0330

    upload project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+    bool gameOver = false;
+    int score = 0;
+    public Text scoreText;
+    public GameObject gameOverPanel;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>().StopSpawing();
+        gameOverPanel.SetActive(true);
+    }
+
+    public void IncerementScore()
+    {
+        if (!gameOver)
+        {
+            score++;
+            print(score);
+
+            scoreText.text = score.ToString();
+        }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    SpriteRenderer sprite;
+    public float xLimit;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlipPlayer();
+    }
+    private void FixedUpdate()
+    {
+
+        float xInput = 0;
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.mousePosition.x < Screen.width / 2)
+            {
+                xInput = -1;
+
+            }
+            else
+            {
+                xInput = 1f;
+            }
+        }
+        else
+        {
+            xInput = 0;
+        }
+        rb.AddForce(transform.right * speed * xInput);
+
+    }
+    void FlipPlayer()
+    {
+        if (rb.velocity.x < 0)
+        {
+            sprite.flipX = true;
+        }
+        else if (rb.velocity.x > 0)
+        {
+            sprite.flipX = false;
+        }
+    }
+}
